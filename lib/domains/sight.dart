3b456adbe34d9a1939ce4f6d2d2f138569134948
denis-tsapalo <TsapaDe@yandex.ru>
2021-06-09 15:04:38 +0300

    Add mock objects

---
+++ lib/domains/sight.dart
@@ -0,0 +1,9 @@
+class Sight {
+  late String name;
+  late double lat, lon;
+  late String url;
+  late String details;
+  late String type;
+
+  Sight(this.name,this.lat, this.lon, this.url, this.details, this.type);
+}
