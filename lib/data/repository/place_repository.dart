358c40285ca635772cd20bd72bdf0d9088a6f6f0
denis-tsapalo <TsapaDe@yandex.ru>
2021-09-16 19:25:59 +0300

    fix

--- lib/data/repository/place_repository.dart
+++ lib/data/repository/place_repository.dart
@@ -15,7 +15,7 @@ BaseOptions baseOptions = BaseOptions(
 
 class PlaceRepository {
   Future<Places> getListPlaces() async {
-    final response = await dio.get<List<dynamic>>('/places');
+    final response = await dio.get<List<dynamic>>('/place');
 
     if (response.statusCode == 200) {
       return Places.fromJson(response.data!);
