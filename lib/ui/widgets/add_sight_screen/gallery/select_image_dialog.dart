4cc653199e466fb0b45e38a4c01072fd6e672712
denis-tsapalo <TsapaDe@yandex.ru>
2021-11-07 10:10:20 +0300

    Added the ability to select an image

--- lib/ui/widgets/add_sight_screen/gallery/select_image_dialog.dart
+++ lib/ui/widgets/add_sight_screen/gallery/select_image_dialog.dart
@@ -1,11 +1,28 @@
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:open_file/open_file.dart';
 import 'package:places/ui/screens/res/colors.dart';
-import 'package:places/ui/screens/res/icons.dart';
 import 'package:places/ui/screens/res/constants.dart' as Constants;
+import 'package:places/ui/screens/res/icons.dart';
+import 'package:places/ui/widgets/add_sight_screen/gallery/file_list.dart';
 
-class SelectImageDialog extends StatelessWidget {
+class SelectImageDialog extends StatefulWidget {
   const SelectImageDialog({Key? key}) : super(key: key);
+
+  @override
+  State<SelectImageDialog> createState() => _SelectImageDialogState();
+}
+
+class _SelectImageDialogState extends State<SelectImageDialog> {
+  File? _image;
+
+  final picker = ImagePicker();
+  FilePickerResult? result;
+  PlatformFile? file;
 
   @override
   Widget build(BuildContext context) {
@@ -25,57 +42,74 @@ class SelectImageDialog extends StatelessWidget {
             ),
             child: Column(
               children: [
-                Row(
-                  children: [
-                    SvgPicture.asset(
-                      iconCamera,
-                      color: myLightSecondaryTwo,
-                    ),
-                    const SizedBox(width: 15),
-                    const Text(
-                      Constants.textBtnCamera,
-                      style: TextStyle(
-                        fontSize: 16,
+                InkWell(
+                  onTap: getImageFromCamera,
+                  child: Row(
+                    children: [
+                      SvgPicture.asset(
+                        iconCamera,
                         color: myLightSecondaryTwo,
                       ),
-                    ),
-                  ],
+                      const SizedBox(width: 15),
+                      const Text(
+                        Constants.textBtnCamera,
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: myLightSecondaryTwo,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
                 const Divider(),
-                Row(
-                  children: [
-                    SvgPicture.asset(
-                      iconPhoto,
-                      color: myLightSecondaryTwo,
-                    ),
-                    const SizedBox(width: 14),
-                    const Text(
-                      Constants.textBtnPhoto,
-                      style: TextStyle(
-                        fontSize: 16,
+                InkWell(
+                  onTap: getImageFromGallery,
+                  child: Row(
+                    children: [
+                      SvgPicture.asset(
+                        iconPhoto,
                         color: myLightSecondaryTwo,
                       ),
-                    ),
-                  ],
-                ),
-                const Divider(),
-                Row(
-                  children: [
-                    const SizedBox(width: 4),
-                    SvgPicture.asset(
-                      iconFile,
-                      color: myLightSecondaryTwo,
-                    ),
-                    const SizedBox(width: 16),
-                    const Text(
-                      Constants.textBtnFile,
-                      style: TextStyle(
-                        fontSize: 16,
-                        color: myLightSecondaryTwo,
+                      const SizedBox(width: 14),
+                      const Text(
+                        Constants.textBtnPhoto,
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: myLightSecondaryTwo,
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
+                // TODO: Clarify the functionality of the "file" button. At the moment it performs actions similar to the "Photo" button.
+                // const Divider(),
+                // InkWell(
+                //   onTap: () async {
+                //     result = await FilePicker.platform
+                //         .pickFiles(type: FileType.image);
+                //     if (result == null) return;
+                //     file = result!.files.first;
+                //     debugPrint(file?.name.toString());
+                //     setState(() {});
+                //   },
+                //   child: Row(
+                //     children: [
+                //       const SizedBox(width: 4),
+                //       SvgPicture.asset(
+                //         iconFile,
+                //         color: myLightSecondaryTwo,
+                //       ),
+                //       const SizedBox(width: 16),
+                //       const Text(
+                //         Constants.textBtnFile,
+                //         style: TextStyle(
+                //           fontSize: 16,
+                //           color: myLightSecondaryTwo,
+                //         ),
+                //       ),
+                //     ],
+                //   ),
+                // ),
               ],
             ),
           ),
@@ -108,4 +142,39 @@ class SelectImageDialog extends StatelessWidget {
       ),
     );
   }
+
+  Future getImageFromGallery() async {
+    final pickedFile = await picker.getImage(source: ImageSource.gallery);
+
+    setState(
+      () {
+        if (pickedFile != null) {
+          _image = File(pickedFile.path);
+        }
+      },
+    );
+  }
+
+  Future getImageFromCamera() async {
+    final pickedFile = await picker.getImage(source: ImageSource.camera);
+
+    setState(() {
+      if (pickedFile != null) {
+        _image = File(pickedFile.path);
+      }
+    });
+  }
+
+  // TODO: Clarify the functionality of the "file" button. At the moment it performs actions similar to the "Photo" button.
+  // multiple file selected
+  // navigate user to 2nd screen to show selected files
+  // void loadSelectedFiles(List<PlatformFile> files) {
+  //   Navigator.of(context).push(MaterialPageRoute(
+  //       builder: (context) => FileList(files: files, onOpenedFile: viewFile)));
+  // }
+
+  // open the picked file
+  // void viewFile(PlatformFile file) {
+  //   OpenFile.open(file.path);
+  // }
 }
